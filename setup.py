f1a67f4be11ad8309341763c7afc4e4002479d27
shunyi <M157q.tw@gmail.com>
2016-09-29 11:33:49 +0800

    Add install_requires for setup.py
    Use requirements.txt for install_requires.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import os
 from setuptools import find_packages, setup
+
+from pip.req import parse_requirements
+
+
+def get_install_reqs():
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    install_requires = parse_requirements(
+        os.path.join(root_dir, 'requirements.txt'), session=False,
+    )
+
+    return [str(ir.req) for ir in install_requires]
+
 
 setup(
     name="drfdocs",
@@ -11,7 +24,7 @@ setup(
     license='BSD',
     description="Documentation for Web APIs made with Django Rest Framework.",
     long_description=open("README.md").read(),
-    install_requires=[],
+    install_requires=get_install_reqs(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
